34586775ff25b413c381e563dd6bc0666eb7093c
elboffin <olamide.adetula.1744@gmail.com>
2023-01-18 14:04:36 +0100

    header

--- nft-marketplace/src/App.jsx
+++ nft-marketplace/src/App.jsx
@@ -1,11 +1,15 @@
 import React from "react"
+import Header from "./components/Header"
 
 function App() {
 
 
   return (
-    <div className="">
-     
+    <div className="min-h-screen">
+      <div className="gradient-bg-hero">
+      <Header/>
+      </div>
+    
     </div>
   )
 }
---
+++ nft-marketplace/src/components/Header.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import nftLogo from "../assets/nft.png"
+
+const Header = () => {
+  return (
+        <div className="w-4/5 flex md:justify-center justify-between
+        items-center py-4 mx-auto" >
+        <div className="md:flex-[0.5] flex-initial justify-center items-center">
+            <img
+                className='w-32 cursor-pointer' src={nftLogo}  alt='Logo' />
+        </div>
+        <ul 
+            className='md:flex-[0.5] text-white md:flex
+            hidden list-none justify-between item-center flex-initial'>
+            <li className='mx-4 cursor-pointer'> Market</li>
+            <li className='mx-4 cursor-pointer'> Artist</li>
+            <li className='mx-4 cursor-pointer'> Features</li>
+            <li className='mx-4 cursor-pointer'> Community</li>
+        </ul>
+        <button  className='shadow-xl shadow-blue-900 text-white ml-8
+        bg-[#b5ba25] hover:bg-[#D3EE70] md:text-xs p-2 
+        rounded-full '>
+           Connect Wallet 
+        </button>
+    </div>
+  )
+}
+
+export default Header
